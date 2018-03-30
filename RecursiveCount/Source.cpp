4cee1ec0aa4295f57efafa4e66bc72471c02e197
Konstantin <konstantin.writing.code@gmail.com>
2018-03-30 23:45:05 +0300

    (fix) support empty array

--- RecursiveCount/Source.cpp
+++ RecursiveCount/Source.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[]) {
 
 	std::function<int(std::vector<int> &)> count = [&](std::vector<int>& arr)
 	{
+		if (arr.size() == 0)
+			return 0;
+
 		if (arr.size() == 1)
 			return 1;
 
@@ -14,11 +17,21 @@ int main(int argc, char *argv[]) {
 		return 1 + count(arr);
 	};
 
-	auto list = std::vector<int>{ 1, 2, 3, 4, 5 };
+	{
+		auto list = std::vector<int>{ 1, 2, 3, 4, 5 };
 
-	auto result = count(list);
+		auto result = count(list);
 
-	std::cout << result << std::endl; // 5
+		std::cout << result << std::endl; // 5
+	}
+
+	{
+		auto list = std::vector<int>{ };
+
+		auto result = count(list);
+
+		std::cout << result << std::endl; // 0
+	}
 
 	return 0;
 }
