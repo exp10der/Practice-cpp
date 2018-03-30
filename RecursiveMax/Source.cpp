353b8f286f23d59b1e5ff6628ec01749891180e3
Konstantin <konstantin.writing.code@gmail.com>
2018-03-30 23:51:49 +0300

    (feature) added checking for an empty array

--- RecursiveMax/Source.cpp
+++ RecursiveMax/Source.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[]) {
 
 	std::function<int(std::vector<int> &)> max = [&](std::vector<int>& arr)
 	{
+		if (arr.size() == 0)
+			throw std::runtime_error("Empty array not supported.");
+
 		if (arr.size() == 1)
 			return arr[0];
 
@@ -17,11 +20,28 @@ int main(int argc, char *argv[]) {
 		return item > resulFn ? item : resulFn;
 	};
 
-	auto list = std::vector<int>{ 1, 2, 3, 4, 5 };
 
-	auto result = max(list);
+	{
+		auto list = std::vector<int>{ 1, 2, 3, 4, 5 };
 
-	std::cout << result << std::endl; // 5
+		auto result = max(list);
+
+		std::cout << result << std::endl; // 5
+	}
+
+	{
+		auto list = std::vector<int>{ };
+
+		try
+		{
+			auto result = max(list);
+		}
+		catch (const std::runtime_error& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	
 
 	return 0;
 }
